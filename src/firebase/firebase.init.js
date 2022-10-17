ec1c0934f07efb1a445e9e270446d1c332256f60
RafiTahmid <rafi.tahmid019@gmail.com>
2022-10-17 13:57:08 +0600

    imported getAuth and firebase settings

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD70-SZ-lV5YK4PTqXfvC68LdEL1WwLoD0",
+  authDomain: "email-pass-auth-1b77a.firebaseapp.com",
+  projectId: "email-pass-auth-1b77a",
+  storageBucket: "email-pass-auth-1b77a.appspot.com",
+  messagingSenderId: "181781966682",
+  appId: "1:181781966682:web:6b1f81a632743fa2b58d63",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
